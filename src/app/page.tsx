224b39db14085a8d1321f6a7e3b5626b3cee0ad2
Amir hanif <amir.hanif1680@gmail.com>
2024-02-25 14:39:44 +0500

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/Hero";
 import ProductList from "@/components/views/ProductList";
 export default function Home() {
   return (
-  <div className="min-h-screen mt-10">
+  <div className="min-h-screen mt-8">
   <Hero/>
   <ProductList/>
   </div>
